6706adef14171d0b70a64f04fdcf4b09fd57fcff
jrharman <joerharman@gmail.com>
2018-01-25 17:12:35 +0000

    Update B1_Gather_Map_statistics.pl

--- Workflow/B1_Gather_Map_statistics.pl
+++ Workflow/B1_Gather_Map_statistics.pl
@@ -2,14 +2,12 @@ use strict;
 use warnings;
 
 ### 10/11/2016
-### Input Mapping output files (The final.out versions)
-### Read key mapping statistics
-### Output to file 
+# Input Mapping output files (The final.out versions)
+# Read key mapping statistics
+# Output to file 
 
 
-###
 ### Setup. Create output file, print headers.
-###
 
 my $count = 1;
 
@@ -17,21 +15,22 @@ if (-e "./StarBAM/Log/outputMappingStats.txt") {
 	system("rm ./StarBAM/Log/outputMappingStats.txt");
 	print "Previous output removed";
 }
+
 open (my $ofh, ">>", "./StarBAM/Log/outputMappingStats.txt") 
 	or die "Cannot open output : $!\n";
 
 print $ofh "Filename\tTotal_Input_Reads\tAvg_read_length\tUniquely_Mapped_Reads\tAvg_Mapped_Length\tMismatch_rate_perBase\tMultimapped_reads\tMultimapped_tooManyLoci\tPercUnmapped_Mismatches\tPercUnmapped_tooShort\tPercUnmapped_other\n";
 
 
-###
 ### Open output file, loop through file lines. Extract stats between lines 21 & 29 and print to file
-###
 
 foreach my $file (@ARGV) {
 	open (my $fh, $file) 
 		or die "Cannot open $file : $!\n";
+		
 	$count = 1;
 	print $ofh $file."\t";
+	
 	while (<$fh>){		
 		if ($count == 6 || $count == 7 || $count == 9 || $count == 11 || $count == 18 || $count == 24 || $count == 26 || $count == 29 || $count == 30 || $count == 31){
 		
