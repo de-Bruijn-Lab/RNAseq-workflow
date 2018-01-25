d342bb4063403b57a8d424092bf32418a0732a98
jrharman <joerharman@gmail.com>
2018-01-25 17:11:33 +0000

    Neatened comments and code

--- Workflow/A2_TrimPlot.R
+++ Workflow/A2_TrimPlot.R
@@ -1,39 +1,38 @@
 ### Plot trimming statistics
-### Simple R script to visualise the effect of trimming on the FastQ files
-###
+# Simple R script to visualise the effect of trimming on the FastQ files
 
-# Read data in
+### Read data in
 
 tbl <- read.table("./trim/trimOutput/outputTrimStats.txt")
 
 
-# Calculate percentage trimming
+### Calculate percentage trimming
 
 PercentTrimmedReads <- tbl[,3]/tbl[,1]*100
 PercentQualityTrimmed <- tbl[,4]/tbl[,2]*100
 PercentTrimmedBases <- tbl[,5]/tbl[,2]*100
 
 
-# Bind statistics, apply averaging
+### Bind statistics to dataframe, apply averaging
 
 stats <- data.frame(PercentTrimmedReads, PercentQualityTrimmed,PercentTrimmedBases)
 avr <- apply(stats,2,mean)
 
 
-# Generate PDF plots
+### Generate PDF plots
 
 pdf("./trim/trimOutput/TrimStats_PercTrimmedReads.pdf",8,8)
-barplot(stats[,1], xlab="FastQ files", ylab="% Trimmed Reads")
+  barplot(stats[,1], xlab="FastQ files", ylab="% Trimmed Reads")
 dev.off()
 
 pdf("./trim/trimOutput/TrimStats_PercQualityTrimmed.pdf",8,8)
-barplot(stats[,2], xlab="FastQ files", ylab="% Quality Trimmed")
+  barplot(stats[,2], xlab="FastQ files", ylab="% Quality Trimmed")
 dev.off()
 
 pdf("./trim/trimOutput/TrimStats_PercTrimmedBases.pdf", 8,8)
-barplot(stats[,3], xlab="FastQ files", ylab="% Trimmed Bases")
+  barplot(stats[,3], xlab="FastQ files", ylab="% Trimmed Bases")
 dev.off()
 
 pdf("./trim/trimOutput/TrimStats_Averages.pdf",8,8)
-barplot(avr, ylab="Percentage", main="Percentage trimming averaged across all files")
+  barplot(avr, ylab="Percentage", main="Percentage trimming averaged across all files")
 dev.off()
