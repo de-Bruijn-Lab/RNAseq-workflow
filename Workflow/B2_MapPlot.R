788813264b1a6a562583fc61bff10e2712874a11
jrharman <joerharman@gmail.com>
2018-01-25 17:17:07 +0000

    Neatened comments and code

--- Workflow/B2_MapPlot.R
+++ Workflow/B2_MapPlot.R
@@ -1,42 +1,49 @@
+### RScript for visualising mapping statistics
+
 library(tools)
 
-# Import output
+### Import mapping statistics
 tbl <- read.table("./StarBAM/Log/outputMappingStats.txt", header=T, row.names=1)
-rownames(tbl) <- basename(file_path_sans_ext(file_path_sans_ext(rownames(tbl))))
+rownames(tbl) <- basename(file_path_sans_ext(file_path_sans_ext(rownames(tbl))))  # Extract file basename
 
-# Read lengths
+### Extract read lengths, and plot
 readLength <- tbl[,c(2,4)]
+
 pdf("./StarBAM/Log/readLength.pdf",8,8)
   boxplot(readLength, main = "Read length")
 dev.off()
 
-# Mapping rate
-
+### Extract mapping rate, and plot
 readMappingPerc <- data.frame(tbl[,3]/tbl[,1]*100, tbl[,6]/tbl[,1]*100, tbl[,8],tbl[,9],tbl[,10])
-colnames(readMappingPerc) <- c("% Uniquely mapped", 
-                               "% Multimapped", 
-                               "% Unmapped - mismatches",
-                               "% Unmapped - short read",
-                               "% Unmapped - other")
+colnames(readMappingPerc) <- c(
+  "% Uniquely mapped", 
+  "% Multimapped", 
+  "% Unmapped - mismatches",
+  "% Unmapped - short read",
+  "% Unmapped - other"
+)
 
 pdf("./StarBAM/Log/mappingRates.pdf",12,8)
   par(mar=c(11,4,4,2))
   boxplot(readMappingPerc, las=2, main="Boxplot of STAR mapping rates", ylab="Percentage")
 dev.off()
 
-# Raw counts
+### Extract raw counts, and plot
 readRaw <- tbl[,c(1,3,6)]
 
 pdf("./StarBAM/Log/rawReadCounts.pdf", 12,8)
   par(mar=c(9,4,6,2))
-  barplot(t(readRaw), 
-          las=2, 
-          main="Barplot of total read counts", 
-          beside=T, 
-          col=c("Black","Grey", "White"),
-          ylim=c(0,max(readRaw)*1.5))
-  legend("topright",
-          cex=0.8,
-          legend=c("Total reads", "Uniquely mapped reads", "Multimapped reads"), 
-          fill=c("Black","Grey", "White")
-)
+  barplot(
+    t(readRaw), 
+    las=2, 
+    main="Barplot of total read counts", 
+    beside=T, 
+    col=c("Black","Grey", "White"),
+    ylim=c(0,max(readRaw)*1.5))
+  legend(
+    "topright",
+    cex=0.8,
+    legend=c("Total reads", "Uniquely mapped reads", "Multimapped reads"), 
+    fill=c("Black","Grey", "White")
+  )
+dev.off()
